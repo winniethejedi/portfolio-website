fe9dd6e528e9605db426c744c46a8b72fe9559b2
winniethejedi <mcraigparker123@gmail.com>
2018-07-01 15:10:17 -0600

    added some jquery goodness

---
+++ main.js
@@ -0,0 +1,10 @@
+const $hide = $('.hide');
+const $header = $('.header');
+const $contents = $('.contents');
+
+
+$(document).ready(() => {
+    $header.on('click', event => {
+        $(event.currentTarget).siblings('.contents').toggleClass('hide').toggleClass('show');
+    }) 
+});
