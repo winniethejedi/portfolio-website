47173b856159cc35e6b5f73d02035bdb03bb44bc
winniethejedi <mcraigparker123@gmail.com>
2018-07-05 20:11:13 -0600

    got the basics working again

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
-global.document = document;
-
-const $ = jQuery = require('jquery')(window);
-
 const $hide = $('.hide');
 const $header = $('.header');
 const $contents = $('.contents');
+const $show = $('.show');
 
 $(document).ready(() => {
-    require('bootstrap');
     $header.on('click', event => {
-        $(event.currentTarget).siblings('.contents').toggleClass('hide');
-    }) 
-
+        console.log($(event.currentTarget));
+        $(event.currentTarget).siblings('.contents').toggleClass('hide').toggleClass('show');
+    });
 
 });
