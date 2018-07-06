1be2129baf0b7eb2680baf037daa02c88322daa8
winniethejedi <mcraigparker123@gmail.com>
2018-07-05 19:26:04 -0600

    I hope I can get this to work later

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+const $ = jQuery = require('jquery')(window);
+
 const $hide = $('.hide');
 const $header = $('.header');
 const $contents = $('.contents');
 
-
 $(document).ready(() => {
+    require('bootstrap');
     $header.on('click', event => {
-        $(event.currentTarget).siblings('.contents').toggleClass('hide', 8000, "easeOutSine");
+        $(event.currentTarget).siblings('.contents').toggleClass('hide');
     }) 
+
+
 });
