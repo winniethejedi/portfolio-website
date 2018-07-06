ac484b22063fd9aff6e9bf93297913f56a2cf440
winniethejedi <mcraigparker123@gmail.com>
2018-07-05 20:55:16 -0600

    started working on menu

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ const $hide = $('.hide');
 const $header = $('.header');
 const $contents = $('.contents');
 const $show = $('.show');
+const $navMenu = $('#nav-menu');
+const $menu = $('#menu');
+
 
 $(document).ready(() => {
     $header.on('click', event => {
-        console.log($(event.currentTarget));
         $(event.currentTarget).siblings('.contents').toggleClass('hide').toggleClass('show');
     });
-
+    $menu.on('click', event => {
+        console.log($(event.currentTarget).siblings('.nav-menu'));
+        $(event.currentTarget).siblings('.nav-menu').toggleClass('hide-menu').toggleClass('show-menu');
+    })
 });
